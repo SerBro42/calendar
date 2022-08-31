b1fa9453f3d80243ed678f07cb314ddcdb9b2514
sergey-dev <serbro7@gmail.com>
2022-08-31 11:08:50 +0200

    Month-week-day view
    Added a functional month, week and day view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CalendarModule } from 'angular-calendar';
-import { DateAdapter } from 'angular-calendar';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { BrowserModule } from '@angular/platform-browser';
+import { CalendarHeaderModuleModule } from './utilities/calendar-header-module/calendar-header-module.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,6 +22,7 @@ import { AppComponent } from './app.component';
       provide: DateAdapter,
       useFactory: adapterFactory,
     }),
+    CalendarHeaderModuleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
